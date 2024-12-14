2c9d3252fe85a57905c1145e21e5b7f18f1d18ac
PiotrWojtalewicz <piowojt19@gmail.com>
2024-12-14 22:09:06 +0100

    dodanie przycisku- zapomniałem hasła

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import login_view, logout_view
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),  # Domyślny widok
     # Widoki dotyczące samochodów
@@ -13,6 +13,10 @@ urlpatterns = [
     #widoki logowania
     path('register/', views.register, name='register'),
     path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
-
+    path('logout/', logout_view, name='logout'),
+    #widoki potrzebne do resetowania hasła
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
